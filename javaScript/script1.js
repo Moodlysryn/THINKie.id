2e75b7c13aa5e7e62828c10dd7c3230b3264286c
Moodlysryn <aqilahwijdan@iqis.sch.id>
2025-11-06 08:57:42 +0800

    perubahan

---
+++ javaScript/script1.js
@@ -0,0 +1,7 @@
+document.querySelector(".create-btn").onclick = () => {
+  alert("Create Account clicked");
+};
+
+document.querySelector(".login-btn").onclick = () => {
+  alert("Log In clicked");
+};
